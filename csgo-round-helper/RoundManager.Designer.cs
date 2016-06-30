f49b5fb4c9ff775edfb929d3f5fa54bb5c4e3415
Mark Anthony Viola <mark.anthony.viola@gmail.com>
2016-06-30 01:53:49 -0400

    Correctly functioning HUD
    Created a HUD that displays the player's current state

--- csgo-round-helper/RoundManager.Designer.cs
+++ csgo-round-helper/RoundManager.Designer.cs
@@ -31,15 +31,33 @@
             this.CTScoreLabel = new System.Windows.Forms.Label();
             this.scoreColonLabel = new System.Windows.Forms.Label();
             this.TScoreLabel = new System.Windows.Forms.Label();
+            this.nameLabel = new System.Windows.Forms.Label();
+            this.primaryTextLabel = new System.Windows.Forms.Label();
+            this.primaryPicBox = new System.Windows.Forms.PictureBox();
+            this.primaryLabel = new System.Windows.Forms.Label();
+            this.secondaryLabel = new System.Windows.Forms.Label();
+            this.secondaryPicBox = new System.Windows.Forms.PictureBox();
+            this.secondaryTextLabel = new System.Windows.Forms.Label();
+            this.nadesTextLabel = new System.Windows.Forms.Label();
+            this.nadePicBox1 = new System.Windows.Forms.PictureBox();
+            this.nadePicBox2 = new System.Windows.Forms.PictureBox();
+            this.nadePicBox3 = new System.Windows.Forms.PictureBox();
+            this.nadePicBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.primaryPicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondaryPicBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nadePicBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nadePicBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nadePicBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nadePicBox4)).BeginInit();
             this.SuspendLayout();
             // 
             // CTScoreLabel
             // 
             this.CTScoreLabel.AutoSize = true;
-            this.CTScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.CTScoreLabel.Location = new System.Drawing.Point(479, 9);
+            this.CTScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CTScoreLabel.Location = new System.Drawing.Point(132, 9);
             this.CTScoreLabel.Name = "CTScoreLabel";
-            this.CTScoreLabel.Size = new System.Drawing.Size(40, 25);
+            this.CTScoreLabel.Size = new System.Drawing.Size(51, 31);
             this.CTScoreLabel.TabIndex = 0;
             this.CTScoreLabel.Text = "CT";
             this.CTScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -47,10 +65,10 @@
             // scoreColonLabel
             // 
             this.scoreColonLabel.AutoSize = true;
-            this.scoreColonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.scoreColonLabel.Location = new System.Drawing.Point(525, 9);
+            this.scoreColonLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreColonLabel.Location = new System.Drawing.Point(178, 9);
             this.scoreColonLabel.Name = "scoreColonLabel";
-            this.scoreColonLabel.Size = new System.Drawing.Size(18, 25);
+            this.scoreColonLabel.Size = new System.Drawing.Size(22, 31);
             this.scoreColonLabel.TabIndex = 1;
             this.scoreColonLabel.Text = ":";
             this.scoreColonLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
@@ -58,24 +76,160 @@
             // TScoreLabel
             // 
             this.TScoreLabel.AutoSize = true;
-            this.TScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TScoreLabel.Location = new System.Drawing.Point(549, 9);
+            this.TScoreLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TScoreLabel.Location = new System.Drawing.Point(206, 9);
             this.TScoreLabel.Name = "TScoreLabel";
-            this.TScoreLabel.Size = new System.Drawing.Size(25, 25);
+            this.TScoreLabel.Size = new System.Drawing.Size(31, 31);
             this.TScoreLabel.TabIndex = 2;
             this.TScoreLabel.Text = "T";
             this.TScoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
-            // RoundHelper
+            // nameLabel
+            // 
+            this.nameLabel.AutoSize = true;
+            this.nameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nameLabel.Location = new System.Drawing.Point(22, 64);
+            this.nameLabel.Name = "nameLabel";
+            this.nameLabel.Size = new System.Drawing.Size(61, 24);
+            this.nameLabel.TabIndex = 3;
+            this.nameLabel.Text = "Name";
+            // 
+            // primaryTextLabel
+            // 
+            this.primaryTextLabel.AutoSize = true;
+            this.primaryTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.primaryTextLabel.Location = new System.Drawing.Point(23, 161);
+            this.primaryTextLabel.Name = "primaryTextLabel";
+            this.primaryTextLabel.Size = new System.Drawing.Size(56, 17);
+            this.primaryTextLabel.TabIndex = 4;
+            this.primaryTextLabel.Text = "Primary";
+            // 
+            // primaryPicBox
+            // 
+            this.primaryPicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.primaryPicBox.Location = new System.Drawing.Point(26, 192);
+            this.primaryPicBox.Name = "primaryPicBox";
+            this.primaryPicBox.Size = new System.Drawing.Size(305, 74);
+            this.primaryPicBox.TabIndex = 5;
+            this.primaryPicBox.TabStop = false;
+            // 
+            // primaryLabel
+            // 
+            this.primaryLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.primaryLabel.Location = new System.Drawing.Point(141, 269);
+            this.primaryLabel.Name = "primaryLabel";
+            this.primaryLabel.Size = new System.Drawing.Size(72, 13);
+            this.primaryLabel.TabIndex = 6;
+            this.primaryLabel.Text = "Primary Name";
+            this.primaryLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // secondaryLabel
+            // 
+            this.secondaryLabel.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.secondaryLabel.Location = new System.Drawing.Point(135, 400);
+            this.secondaryLabel.Name = "secondaryLabel";
+            this.secondaryLabel.Size = new System.Drawing.Size(89, 13);
+            this.secondaryLabel.TabIndex = 9;
+            this.secondaryLabel.Text = "Secondary Name";
+            this.secondaryLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // secondaryPicBox
+            // 
+            this.secondaryPicBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.secondaryPicBox.Location = new System.Drawing.Point(26, 321);
+            this.secondaryPicBox.Name = "secondaryPicBox";
+            this.secondaryPicBox.Size = new System.Drawing.Size(305, 74);
+            this.secondaryPicBox.TabIndex = 8;
+            this.secondaryPicBox.TabStop = false;
+            // 
+            // secondaryTextLabel
+            // 
+            this.secondaryTextLabel.AutoSize = true;
+            this.secondaryTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.secondaryTextLabel.Location = new System.Drawing.Point(23, 290);
+            this.secondaryTextLabel.Name = "secondaryTextLabel";
+            this.secondaryTextLabel.Size = new System.Drawing.Size(76, 17);
+            this.secondaryTextLabel.TabIndex = 7;
+            this.secondaryTextLabel.Text = "Secondary";
+            // 
+            // nadesTextLabel
+            // 
+            this.nadesTextLabel.AutoSize = true;
+            this.nadesTextLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nadesTextLabel.Location = new System.Drawing.Point(23, 440);
+            this.nadesTextLabel.Name = "nadesTextLabel";
+            this.nadesTextLabel.Size = new System.Drawing.Size(71, 17);
+            this.nadesTextLabel.TabIndex = 10;
+            this.nadesTextLabel.Text = "Grenades";
+            // 
+            // nadePicBox1
+            // 
+            this.nadePicBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nadePicBox1.Location = new System.Drawing.Point(32, 479);
+            this.nadePicBox1.Name = "nadePicBox1";
+            this.nadePicBox1.Size = new System.Drawing.Size(68, 66);
+            this.nadePicBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.nadePicBox1.TabIndex = 11;
+            this.nadePicBox1.TabStop = false;
+            // 
+            // nadePicBox2
+            // 
+            this.nadePicBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nadePicBox2.Location = new System.Drawing.Point(106, 479);
+            this.nadePicBox2.Name = "nadePicBox2";
+            this.nadePicBox2.Size = new System.Drawing.Size(68, 66);
+            this.nadePicBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.nadePicBox2.TabIndex = 12;
+            this.nadePicBox2.TabStop = false;
+            // 
+            // nadePicBox3
+            // 
+            this.nadePicBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nadePicBox3.Location = new System.Drawing.Point(180, 479);
+            this.nadePicBox3.Name = "nadePicBox3";
+            this.nadePicBox3.Size = new System.Drawing.Size(68, 66);
+            this.nadePicBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.nadePicBox3.TabIndex = 13;
+            this.nadePicBox3.TabStop = false;
+            // 
+            // nadePicBox4
+            // 
+            this.nadePicBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nadePicBox4.Location = new System.Drawing.Point(254, 479);
+            this.nadePicBox4.Name = "nadePicBox4";
+            this.nadePicBox4.Size = new System.Drawing.Size(68, 66);
+            this.nadePicBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.nadePicBox4.TabIndex = 14;
+            this.nadePicBox4.TabStop = false;
+            // 
+            // RoundManager
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1106, 539);
+            this.ClientSize = new System.Drawing.Size(356, 589);
+            this.Controls.Add(this.nadePicBox4);
+            this.Controls.Add(this.nadePicBox3);
+            this.Controls.Add(this.nadePicBox2);
+            this.Controls.Add(this.nadePicBox1);
+            this.Controls.Add(this.nadesTextLabel);
+            this.Controls.Add(this.secondaryLabel);
+            this.Controls.Add(this.secondaryPicBox);
+            this.Controls.Add(this.secondaryTextLabel);
+            this.Controls.Add(this.primaryLabel);
+            this.Controls.Add(this.primaryPicBox);
+            this.Controls.Add(this.primaryTextLabel);
+            this.Controls.Add(this.nameLabel);
             this.Controls.Add(this.TScoreLabel);
             this.Controls.Add(this.scoreColonLabel);
             this.Controls.Add(this.CTScoreLabel);
-            this.Name = "RoundHelper";
+            this.Name = "RoundManager";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.primaryPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.secondaryPicBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nadePicBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nadePicBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nadePicBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nadePicBox4)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,6 +240,18 @@
         private System.Windows.Forms.Label CTScoreLabel;
         private System.Windows.Forms.Label scoreColonLabel;
         private System.Windows.Forms.Label TScoreLabel;
+        private System.Windows.Forms.Label nameLabel;
+        private System.Windows.Forms.Label primaryTextLabel;
+        private System.Windows.Forms.PictureBox primaryPicBox;
+        private System.Windows.Forms.Label primaryLabel;
+        private System.Windows.Forms.Label secondaryLabel;
+        private System.Windows.Forms.PictureBox secondaryPicBox;
+        private System.Windows.Forms.Label secondaryTextLabel;
+        private System.Windows.Forms.Label nadesTextLabel;
+        private System.Windows.Forms.PictureBox nadePicBox1;
+        private System.Windows.Forms.PictureBox nadePicBox2;
+        private System.Windows.Forms.PictureBox nadePicBox3;
+        private System.Windows.Forms.PictureBox nadePicBox4;
     }
 }
 
--- csgo-round-helper/RoundManager.cs
+++ csgo-round-helper/RoundManager.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 using CSGSI;
 using CSGSI.Nodes;
 
@@ -24,29 +25,134 @@ namespace csgo_round_helper
             {
                 Environment.Exit(0);
             }
-            Console.WriteLine("Listening...");
+           
         }
 
-        bool IsPlanted = false;
+        bool plantedThisRound = false;
+        String userID = null;
 
         void OnNewGameState(GameState gs)
         {
-            CTScoreLabel.Text = gs.Map.TeamCT.Score.ToString();
-            TScoreLabel.Text = gs.Map.TeamT.Score.ToString();
-            Console.WriteLine("writing...");
 
-            if (!IsPlanted &&
-               gs.Round.Phase == RoundPhase.Live &&
-               gs.Round.Bomb == BombState.Planted &&
-               gs.Previously.Round.Bomb == BombState.Undefined)
+            if(userID == null)
+            {
+                userID = gs.Player.SteamID;
+                nameLabel.Text = gs.Player.Name;
+            }
+
+
+            if (!plantedThisRound && gs.Round.Phase == RoundPhase.Live &&
+               gs.Round.Bomb == BombState.Planted && gs.Previously.Round.Bomb == BombState.Undefined)
             {
                 Console.WriteLine("Bomb has been planted.");
-                IsPlanted = true;
+                plantedThisRound = true;
             }
-            else if (IsPlanted && gs.Round.Phase == RoundPhase.FreezeTime)
+
+            // Update interface state when round ends
+            if(gs.Round.Phase == RoundPhase.Over)
             {
-                IsPlanted = false;
+                plantedThisRound = false;
+                CTScoreLabel.Text = gs.Map.TeamCT.Score.ToString();
+                TScoreLabel.Text = gs.Map.TeamT.Score.ToString();
+            }
+
+            // Check if the user is the player being observed
+            if(userID == gs.Player.SteamID)
+            {
+                bool havePrimary = false, haveSecondary = false;
+                List<WeaponNode> grenades = new List<WeaponNode>();
+                for (int i = 0; i < gs.Player.Weapons.Count; i++)
+                {
+                    WeaponType weaponType = gs.Player.Weapons[i].Type;
+                    if(weaponType == WeaponType.MachineGun || weaponType == WeaponType.Rifle || weaponType == WeaponType.Shotgun
+                        || weaponType == WeaponType.SniperRifle || weaponType == WeaponType.SubmachineGun)
+                    {
+                        setPrimary(gs.Player.Weapons[i]);
+                        havePrimary = true;
+                    }
+                    else if(weaponType == WeaponType.Pistol)
+                    {
+                        setSecondary(gs.Player.Weapons[i]);
+                        haveSecondary = true;
+                    }
+                    else if(weaponType == WeaponType.Grenade)
+                    {
+                        grenades.Add(gs.Player.Weapons[i]);
+                        setGrenades(grenades);
+                    }
+                }
+                clearWeapons(havePrimary, haveSecondary);
+            }
+           
+        }
+
+        // Set the image of the primary weapon that the player owns
+        void setPrimary(WeaponNode primary)
+        {
+            String name = primary.Name;
+            String picPath = System.AppDomain.CurrentDomain.BaseDirectory + "imgs/" + name + ".png";
+            if(File.Exists(picPath))
+            {
+                primaryPicBox.Image = Image.FromFile(picPath);
+                primaryLabel.Text = name.Substring(7);
             }
         }
+
+        // Set the image of the secondary weapon that the player owns
+        void setSecondary(WeaponNode secondary)
+        {
+            String name = secondary.Name;
+            String picPath = System.AppDomain.CurrentDomain.BaseDirectory + "imgs/" + name + ".png";
+            if (File.Exists(picPath))
+            {
+                secondaryPicBox.Image = Image.FromFile(picPath);
+                secondaryLabel.Text = name.Substring(7);
+            }
+        }
+
+        // Set the images for the grenades the player owns
+        void setGrenades(List<WeaponNode> grenades)
+        {
+            List<PictureBox> grenadePicBoxes = new List<PictureBox>();
+            grenadePicBoxes.Add(nadePicBox1);
+            grenadePicBoxes.Add(nadePicBox2);
+            grenadePicBoxes.Add(nadePicBox3);
+            grenadePicBoxes.Add(nadePicBox4);
+
+            for (int i = 0; i < 4; i++)
+            {
+                if(i < grenades.Count)
+                {
+                    String picPath = System.AppDomain.CurrentDomain.BaseDirectory + "imgs/" +
+                    grenades.ElementAt(i).Name + ".png";
+                    if (File.Exists(picPath))
+                    {
+                        grenadePicBoxes.ElementAt(i).Image = Image.FromFile(picPath);
+                    }
+                }
+                else
+                {
+                    grenadePicBoxes.ElementAt(i).Image = null;
+                }
+            }
+        }
+
+        // Empty the PictureBox if the player doesn't own the corresponding weapon
+        void clearWeapons(bool havePrimary, bool haveSecondary)
+        {
+            if(!havePrimary)
+            {
+                primaryPicBox.Image = null;
+            }
+
+            if (!haveSecondary)
+            {
+                secondaryPicBox.Image = null;
+            }
+        }
+
     }
+
 }
+
+
